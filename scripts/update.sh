4ed41baeeadf19440e6f3584e0806a243ff8b7af
bdistin <bdistin@gmail.com>
2025-05-22 17:06:05 -0500

    update services

--- scripts/update.sh
+++ scripts/update.sh
@@ -24,5 +24,41 @@ echo -e "${INFO}Updating Repo...${RESET}"
 cd $PIOSK_DIR
 git pull
 
+echo -e "${INFO}Installing PiOSK services...${RESET}"
+PI_USER="$SUDO_USER"
+PI_SUID=$(id -u "$SUDO_USER")
+PI_HOME=$(eval echo ~"$SUDO_USER")
+
+sed -e "s|PI_HOME|$PI_HOME|g" \
+	-e "s|PI_SUID|$PI_SUID|g" \
+	-e "s|PI_USER|$PI_USER|g" \
+	"$PIOSK_DIR/services/piosk-runner.template" > "/etc/systemd/system/piosk-runner.service"
+
+sed -e "s|PI_HOME|$PI_HOME|g" \
+	-e "s|PI_SUID|$PI_SUID|g" \
+	-e "s|PI_USER|$PI_USER|g" \
+	"$PIOSK_DIR/services/piosk-switcher.template" > "/etc/systemd/system/piosk-switcher.service"
+
+cp "$PIOSK_DIR/services/piosk-dashboard.template" /etc/systemd/system/piosk-dashboard.service
+cp "$PIOSK_DIR/services/piosk-video.template" /etc/systemd/system/piosk-video.service
+cp "$PIOSK_DIR/services/piosk-wlan0pwr.template" /etc/systemd/system/piosk-wlan0pwr.service
+
+echo -e "${INFO}Reloading systemd daemons...${RESET}"
+systemctl daemon-reload
+
+echo -e "${INFO}Enabling PiOSK daemons...${RESET}"
+systemctl enable piosk-runner
+systemctl enable piosk-switcher
+systemctl enable piosk-dashboard
+systemctl enable piosk-video
+systemctl enable piosk-wlan0pwr
+
+echo -e "${INFO}Starting PiOSK daemons...${RESET}"
+# The runner and switcher services are meant to be started after reboot
+# systemctl start piosk-runner
+# systemctl start piosk-switcher
+systemctl start piosk-dashboard
+systemctl start piosk-video
+
 echo -e "${SUCCESS}\tUpdate done! Restarting...${RESET}"
 reboot
