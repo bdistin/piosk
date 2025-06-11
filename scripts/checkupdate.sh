9766804cb43fd6c92ae9085a2c758b27b536724d
bdistin <bdistin@gmail.com>
2025-06-11 16:53:18 -0500

    branch agnostic with branch notifications

--- scripts/checkupdate.sh
+++ scripts/checkupdate.sh
@@ -6,14 +6,17 @@ git fetch origin
 # Get the current local commit hash
 local_commit=$(git rev-parse HEAD)
 
+# Get the local branch name
+local_branch=$(git rev-parse --abbrev-ref HEAD)
+
 # Get the latest remote commit hash
-remote_commit=$(git rev-parse origin/main)
+remote_commit=$(git rev-parse origin/$local_branch)
 
 # Compare the two hashes
 if [ "$local_commit" = "$remote_commit" ]; then
-  echo "PiOSK is up to date."
+  echo "PiOSK:$local_branch is up to date."
 elif git merge-base --is-ancestor "$local_commit" "$remote_commit"; then
-  echo "There is an update available for PiOSK."
+  echo "There is an update available for PiOSK:$local_branch."
 else
-  echo "The local PiOSK is ahead of the online version. Fix this by connecting to ssh and running `git reset --hard origin/main`"
+  echo "The local PiOSK is ahead of the online version. Fix this by connecting to ssh and running `git reset --hard origin/$local_branch`"
 fi
