8f825d067fb5a344c4dcb3a5b8eeae3cef06947e
bdistin <bdistin@gmail.com>
2025-06-19 16:55:13 -0500

    idle never changed in the loop, fixed

--- scripts/switcher.sh
+++ scripts/switcher.sh
@@ -4,10 +4,10 @@
 export XDG_RUNTIME_DIR=/run/user/1000
 
 IDLE_TIMEOUT=$(jq -r '.page_timeout' /opt/piosk/config.json)
-IDLE=$(xprintidle) / 1000
 
 while true
 do
+	IDLE=$(xprintidle) / 1000
 	if [ $IDLE -ge $IDLE_TIMEOUT ]; then
 		curl -X POST http://localhost/refresh
 		sleep $IDLE_TIMEOUT
